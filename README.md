32e11ced943cea891b3c5a91ad6772760a54c5ce
alejandro <tzalejo@gmail.com>
2020-12-22 00:41:20 -0300

    conociendo la Inyección de Dependencias

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ Existen dos tipos de inyección, el primer tipo es a través del constructor de 
 
 En palabras simples la inyección de dependencias te permite pasar a través del constructor de la clase todos los objetos que necesita tu clase para funcionar.
 
+# Ventajas de usar la Inyección de Dependencias
+Como ventajas podemos nombrar:
+
+- Nuestro código se vuelve más modular y menos acoplado, ya que los submódulos no necesitan saber detalles de la implementación de otros en tu código.
+- Gracias a esta modularidad es que podemos hacer tests unitarios más fácilmente, cada parte del programa se puede aislar porque para realizar pruebas no tenemos que proveer a nuestros programas de las referencias reales, sino que podemos fingirlas mediante mocks.
+- La inyección de dependencias puede hacer que escribas código más rápido y te quita preocupaciones de tener que instanciar objetos tu mismo.
--- UserController.php
+++ UserController.php
@@ -37,3 +37,18 @@ class UserController
     return $this->response('Usuario registrado');
   }
 }
+/**
+ * El problema con la implementación anterior, es que la clase “DataBase” 
+ * se declara implícitamente o dicho de otra forma: No es posible saber que 
+ * existe dicho objeto hasta inspeccionar código fuente de la clase UserController. 
+ * Por lo tanto no solamente la declaración es implícita sino que las clases 
+ * también están acopladas.
+ * 
+ * Es decir, que no puedes sustituir la clase DataBase, por PostgresDatabase o 
+ * MySQLDatabase sin modificar el código de la clase UserController. De ésta 
+ * misma manera, la clase se vuelve difícil de mantener y difícil de depurar. 
+ * Es aquí donde entra en juego la inyección de dependencia, ya que cuando utilizas 
+ * éste patrón estás haciendo explicita la declaración de la clase y por lo tanto 
+ * desacoplando UserController de DataBase. 
+ * 
+ */
